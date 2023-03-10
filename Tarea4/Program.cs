34dd1e205d96daf83d1425a5463f265ee4611601
Henriquez77 <jhonatanernesto.henrriquez@gmail.com>
2023-03-10 10:09:39 -0600

    Ejercicio for Jhonatan Henriquez

---
+++ Tarea4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Tarea4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingresa el tamaño del triangulo a generar: ");
+            var tamaño = int.Parse(Console.ReadLine());
+            //i columnas
+            //j filas
+            for (int i = 0; i < tamaño; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("");
+            }
+        }
+    }
+}
